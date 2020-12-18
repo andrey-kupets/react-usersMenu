d96bda1a793b113f501ef70f1161fba2e521243f
freefireman <beluh.c.s@gmail.com>
2020-12-18 18:55:52 +0200

    part of a word starting with 'use' React understands how to use hooks

--- src/services/UseFetch.js
+++ src/services/UseFetch.js
@@ -1,4 +1,4 @@
-export default function useFetch(url) {
+export default function doFetch(url) {
     const baseUrl = 'https://jsonplaceholder.typicode.com';
     return fetch(baseUrl + url).then(response => response.json());
 }
